3c6eb5b121edc22111f955f54051cd26798bfa69
ket163 <hovmfa@yandex.ru>
2018-06-28 08:54:22 +0300

    Add files via upload

---
+++ JenkinsNallure/src/test/java/utils/TestPrepare.java
@@ -0,0 +1,46 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class TestPrepare {
+
+    static WebDriver driver;
+
+    public static WebDriver getInstance() {
+        if (driver == null) {
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+
+
+    @BeforeClass
+    public void SetupTests() {
+        WebDriverManager.chromedriver().setup();
+        getInstance();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void AfterTests() {
+        if (driver == null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
